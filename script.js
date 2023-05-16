ae4b0415f1b5758d09a20e402aa4e6298d5bcec6
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2023-05-16 00:27:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -61,12 +61,13 @@ function adicionarBoleto(nomeBoleto, boletoNumber, dataVencimento) {
 
 // Armazena o boleto no localStorage
 var boletos = obterBoletos();
-boletos.push({
+var novoBoleto = {
     nome: nomeBoleto,
     boleto: boletoNumber,
     dataVencimento: dataVencimento.toISOString(), // Armazena a data como string no formato ISO
     categoria: categoria // Adiciona a categoria ao objeto do boleto
-});
+};
+boletos.push(novoBoleto);
 localStorage.setItem('boletos', JSON.stringify(boletos));
 
 atualizarListaBoletos();
@@ -84,36 +85,80 @@ if (boletos) {
 
 // Ao carregar a página
 window.addEventListener('load', function() {
-atualizarListaBoletos();
-});
-
-// Função para atualizar a lista de boletos na página
-function atualizarListaBoletos() {
+    atualizarListaBoletos();
+  });
+  
+  // Função para atualizar a lista de boletos na página
+  function atualizarListaBoletos() {
     var listaBoletos = document.getElementById('lista-boletos');
     listaBoletos.innerHTML = ''; // Limpa a lista de boletos
-
+  
     var boletos = obterBoletos();
-
+  
     // Ordena os boletos com base na data de vencimento
     boletos.sort(function(a, b) {
-        var dataA = new Date(a.dataVencimento);
-        var dataB = new Date(b.dataVencimento);
-        return dataA - dataB;
+      var dataA = new Date(a.dataVencimento);
+      var dataB = new Date(b.dataVencimento);
+      return dataA - dataB;
     });
-
+  
     boletos.forEach(function(boleto) {
-        var itemBoleto = document.createElement('li');
-        itemBoleto.textContent = `${boleto.nome}: ${boleto.boleto} (Vencimento: ${new Date(boleto.dataVencimento).toLocaleDateString()})`;
-
-        // Aplica a classe correta ao item do boleto com base na categoria
-        if (boleto.categoria === 'vencidas') {
-            itemBoleto.classList.add('vermelho');
-        } else if (boleto.categoria === 'prestes-a-vencer') {
-            itemBoleto.classList.add('amarelo');
-        } else {
-            itemBoleto.classList.add('azul');
-        }
-
-        listaBoletos.appendChild(itemBoleto);
+      var itemBoleto = document.createElement('li');
+      itemBoleto.textContent = `${boleto.nome}: ${boleto.boleto} (Vencimento: ${new Date(boleto.dataVencimento).toLocaleDateString()})`;
+  
+      // Aplica a classe correta ao item do boleto com base na categoria
+      if (boleto.categoria === 'vencidas') {
+        itemBoleto.classList.add('vermelho');
+      } else if (boleto.categoria === 'prestes-a-vencer') {
+        itemBoleto.classList.add('amarelo');
+      } else {
+        itemBoleto.classList.add('azul');
+      }
+  
+      // Cria o botão de exclusão
+      var botaoExcluir = document.createElement('button');
+      botaoExcluir.textContent = 'Excluir';
+      botaoExcluir.setAttribute('data-id', boleto.id); // Define o atributo de identificador único
+  
+      // Registra o evento de clique no botão de exclusão
+      botaoExcluir.addEventListener('click', function() {
+        var boletoId = this.getAttribute('data-id');
+        excluirBoleto(boletoId);
+      });
+  
+      // Adiciona o botão de exclusão ao item do boleto
+      itemBoleto.appendChild(botaoExcluir);
+  
+      // Adiciona o item do boleto à lista
+      listaBoletos.appendChild(itemBoleto);
     });
-}
+  }
+  
+  // Função para excluir um boleto da lista
+function excluirBoleto(boletoId) {
+    // Obtém os boletos do armazenamento local
+    var boletos = obterBoletos();
+  
+    // Procura o boleto pelo identificador único
+    var boletoIndex = boletos.findIndex(function(boleto) {
+      return boleto.id === boletoId;
+    });
+  
+    // Verifica se o boleto foi encontrado
+    if (boletoIndex !== -1) {
+      // Exibe uma mensagem de confirmação antes da exclusão
+      var confirmacao = confirm('Tem certeza que deseja excluir este boleto?');
+  
+      // Verifica se o usuário confirmou a exclusão
+      if (confirmacao) {
+        // Remove o boleto da lista
+        boletos.splice(boletoIndex, 1);
+  
+        // Atualiza o armazenamento local com a lista atualizada de boletos
+        localStorage.setItem('boletos', JSON.stringify(boletos));
+  
+        // Atualiza a exibição da lista de boletos na página
+        atualizarListaBoletos();
+      }
+    }
+  }
